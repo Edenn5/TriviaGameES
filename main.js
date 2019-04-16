dbf3a9c8d9176e528f7dad286b9eb335a6e63c13
Sobalvarro <0337208@students.dadeschools.net>
2019-04-16 12:01:09 -0400

    Finished html and started js

---
+++ main.js
@@ -0,0 +1,22 @@
+var panel = $("#quiz-area");
+
+var countStartNumber = 30;
+
+// Question Set
+var questions = [
+  
+    {
+        question: "Who won the first world cup?" , 
+        answers: ["Argentina", "Italy", "Uruguay", "Brazil"],
+        correctAnswer: "Uruguay" , 
+        image: "assests\Uruguay.gif"
+    },
+
+    {
+        question: "Who is the top goal scorer in world cup history?" , 
+        answers: ["Just Fontaine", "Muller", "Klose", "Ronaldo"],
+        correctAnswer: "Klose" , 
+        image: "assests\klose.gif"
+    },
+
+]
