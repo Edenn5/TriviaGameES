876798523a21af42c48d7c3ef4875f21cd83bd22
Sobalvarro <0337208@students.dadeschools.net>
2019-04-25 11:12:55 -0400

    finished game object

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ var game = {
     loadQuestion: function() {
         timer = setInterval(game.countdown, 1000);
         panel.html("<h2>" +  questions[this.currentQuestion].question + "</h2>" );
-        for (var i = 0; i < question[this.currentQuestion].answers.length; i++)
+        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++)
         {
             panel.append("<button class='answer-button' id='button' data name= ''" + 
             questions[this.currentQuestion].answers[i] + "'>"+
@@ -95,4 +95,70 @@ var game = {
 
     },
 
-}
+    clicked: function(e) {
+
+        clearInterval(timer);
+
+        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer)
+        {
+            this.answeredCorrectly();
+        }
+        else
+        {
+            this.answeredIncorrectly();
+        }
+
+    },
+
+    answeredIncorrectly: function() {
+
+        game.incorrect++;
+
+        clearInterval(timer);
+
+        panel.html("<h2>Nope!</h2>");
+        panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>")
+        panel.append("<img src='" + questions.[game.currentQuestion].image + "'/>")
+
+        
+        if (game.currentQuestion === questions.length -1)
+        {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else{
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+
+    },
+
+    answeredCorrectly: function() {
+
+        game.correct++;
+
+        clearInterval(timer);
+
+        panel.html("<h2>Correct!</h2>");
+    
+        panel.append("<img src='" + questions.[game.currentQuestion].image + "'/>")
+
+        
+        if (game.currentQuestion === questions.length -1)
+        {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else{
+            setTimeout(game.nextQuestion, 3 * 1000);
+        } 
+    },
+
+    reset: function() {
+
+        this.currentQuestion = 0;
+        this.counter = countStartNumber;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.loadQuestion = 0;
+
+    }
+
+};
